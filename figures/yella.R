299aadd6fa8839bb323018ff7a90cb5d23333ef0
Dennis Evangelista <devangel77b@gmail.com>
2026-02-17 06:11:15 -0500

    figure1

--- figures/yella.R
+++ figures/yella.R
@@ -12,10 +12,16 @@ v_predicted <- function(height_m,g){
 
 # make plots
 fig <- ggplot(data)+
+    geom_hline(yintercept=0,color='gray50')+
     geom_point(aes(x=height_m,y=v_ms,color=type))+
     geom_function(fun=v_predicted,args=list(g=6.91),color='pink')+
-    geom_function(fun=v_predicted,args=list(g=10.18),color='cyan')
-# format and adjust sizes etc for print
+    geom_function(fun=v_predicted,args=list(g=10.18),color='cyan')+
+    theme_classic(base_size=8)+
+    xlab('$h$ (\\unit{\\meter})')+
+    ylab('$v$ (\\unit{\\meter\\per\\second})')
+# format legend position later
+# save the figure
+ggsave("fig1.svg",plot=fig,width=3.4167,height=2,units="in")
 
 
 # do stats to get g and compare
@@ -27,4 +33,5 @@ model2 <- lm(vsquared~height_m:type+0,data)
 anova(model1,model2)
 
 # get g for cricket versus ping pong cases
-# LATER
+print(summary(lm(vsquared~height_m+0,filter(data,type=='ping_pong'))))
+print(summary(lm(vsquared~height_m+0,filter(data,type=='cricket'))))
